ca9baa8b96325e23f5154bc76fb7d1c5ae03e386
r.achanova <r.achanova@pmg-pernik.com>
2021-08-17 15:44:28 +0300

    Added HomeControllerSystemTest

---
+++ BeekeepingDiary/BeekeepingDiary.Tests/Controllers/HomeControllerSystemTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BeekeepingDiary.Tests.Controllers
+{
+  public class HomeControllerSystemTest: IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> factory;
+
+        public HomeControllerSystemTest(WebApplicationFactory<Startup> factory)
+            => this.factory = factory;
+
+        [Fact]
+        public async Task IndexShouldReturnCorrectStatusCode()
+        {
+            // Arrange
+            var client = this.factory.CreateClient();
+
+            // Act
+            var result = await client.GetAsync("/");
+
+            // Arrange
+            Assert.True(result.IsSuccessStatusCode);
+        }
+    }
+}
